aa128591b29e290bec25e20a4c3ca532954c4089
Akash Rajguru <akash.rajguru@est.tech>
2023-07-03 00:06:13 +0100

    commit for presubmits job

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	a := nmath.Add(1, 2)
+	a := nmath.Add(1, 4)
 	fmt.Println("One plus two seems to be:", a)
 	fmt.Println("One plus two seems to be:", a)
-	a = nmath.Multiply(3, 2)
+	a = nmath.Multiply(2, 2)
 	fmt.Println("Two multiply two seems to be:", a)
 }
